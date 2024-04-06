93f6ce60aac773643ee1ec9845f7920a3b53ceef
YouChen <kalibug@qq.com>
2024-04-06 20:31:13 +0800

    update

---
+++ recovery.sh
@@ -0,0 +1,9 @@
+BASE_PATH="$HOME/osmedeus-base"
+
+echo "开始更新workflow"
+rm -rf $BASE_PATH/workflow >/dev/null 2>&1
+git clone --quiet --depth=1 https://github.com/osmedeus/osmedeus-workflow $BASE_PATH/workflow
+## retry to clone in case of anything wrong with the connection
+if [ ! -d "$BASE_PATH/workflow" ]; then
+    git clone --quiet --depth=1 https://github.com/osmedeus/osmedeus-workflow $BASE_PATH
+fi
