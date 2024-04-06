2f57d5687f564eb0c2275e089458e3fdde14d87e
YouChen <kalibug@qq.com>
2024-04-06 22:11:04 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -2,8 +2,8 @@ BASE_PATH="$HOME/osmedeus-base"
 
 echo "开始更新workflow"
 rm -rf $BASE_PATH/workflow >/dev/null 2>&1
-git clone --quiet --depth=1 https://github.com/YouChenJun/Scan-workflow/tree/test $BASE_PATH/workflow
+git clone -b test --quiet --depth=1 https://github.com/YouChenJun/Scan-workflow $BASE_PATH/workflow
 ## retry to clone in case of anything wrong with the connection
 if [ ! -d "$BASE_PATH/workflow" ]; then
-    git clone --quiet --depth=1 https://github.com/YouChenJun/Scan-workflow/tree/test $BASE_PATH
+    git clone -b test --quiet --depth=1 https://github.com/YouChenJun/Scan-workflow $BASE_PATH
 fi
